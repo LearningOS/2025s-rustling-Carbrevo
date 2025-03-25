73a55044f1e52ee62f8ba71de4272602b3269913
Carbrevo <carbrevo@hotmail.com>
2025-03-25 23:19:54 +0800

    0325

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -2,7 +2,6 @@
 	graph
 	This problem requires you to implement a basic graph functio
 */
-// I AM NOT DONE
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
@@ -30,6 +29,18 @@ impl Graph for UndirectedGraph {
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        if !self.contains(edge.0) {
+            self.adjacency_table.insert(edge.0.to_string(), vec![(edge.1.to_string(), edge.2)]);
+        } else {
+            let value = self.adjacency_table.get_mut(edge.0).unwrap();
+            value.push((edge.1.to_string(), edge.2));
+        }
+        if !self.contains(edge.1) {
+            self.adjacency_table.insert(edge.1.to_string(), vec![(edge.0.to_string(), edge.2)]);
+        } else {
+            let value = self.adjacency_table.get_mut(edge.1).unwrap();
+            value.push((edge.0.to_string(), edge.2));
+        }
     }
 }
 pub trait Graph {
